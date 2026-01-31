6ce000723f698707eaae7acf70c501828d8e52d7
_kentho <kenneththomsennmbk@gmail.com>
2026-01-30 23:33:02 -0800

    Added Express server file.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Serve all files in the blnksh0r3 dir
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+console.log('server running');
+}); 
